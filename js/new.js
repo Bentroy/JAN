aac2bbdb2f08e4d4cb604b8f3681d85c8e25782d
Bentroy <bentroy006@gmail.com>
2025-01-08 16:05:51 +0100

    hmm

--- js/new.js
+++ js/new.js
@@ -7,22 +7,26 @@ let car = {
 
 //Array Methods
 
-//push
+//push adds one element to the array or adds elements to an empty array
 let fruits = [];
 fruits.push("apple", "banana", "mango", "orange", "pineapple");
 console.log(fruits);
 
 
 
-//pop
+//pop basically removes the last element in the array
 fruits.pop(2,1, "mango", "orange");
 console.log(fruits);
 
-//Shift
+//Shift removes the first element and brings the the others to a lower index
 fruits.shift();
 console.log(fruits);
 
 
-//unshift
+//unshift add an element to the begining of an array
 fruits.unshift("KIWI");
 console.log(fruits);
+
+//Splice adds, removes or replace an element on the array
+
+//map 
