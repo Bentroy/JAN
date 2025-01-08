1378b5756841a090ac28c2c8756f6389cd4f5de1
Bentroy <bentroy006@gmail.com>
2025-01-08 15:55:36 +0100

    unshift

--- js/new.js
+++ js/new.js
@@ -5,7 +5,24 @@ let car = {
 };
 /*assignment, building a simple calculator with html, css and javascript */
 
+//Array Methods
 
+//push
 let fruits = [];
 fruits.push("apple", "banana", "mango", "orange", "pineapple");
+console.log(fruits);
+
+
+
+//pop
+fruits.pop(2,1, "mango", "orange");
+console.log(fruits);
+
+//Shift
+fruits.shift();
+console.log(fruits);
+
+
+//unshift
+fruits.unshift("KIWI");
 console.log(fruits);
