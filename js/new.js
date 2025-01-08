71a13d02ac7e84b2d0e1e439f7a6b08529c1b1a0
Bentroy <bentroy006@gmail.com>
2025-01-08 15:33:16 +0100

    js

---
+++ js/new.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let car = {
+    name: "toyota",
+};
+/*assignment, building a simple calculator with html, css and javascript */
+
+
+let fruits = [];
+fruits.push("apple", "banana", "mango", "orange", "pineapple");
+console.log(fruits);
